01eb48e74f5534f1e61046bf7a60bb4d72173ac8
zhoudongfei <1371105743@qq.com>
2020-11-18 23:31:08 +0800

    新增路由功能，以及注册页面

---
+++ src/components/layouts/TheEntry.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navbar-right">
+        <div class="nav navbar-nav github-login">
+            <a href="#" class="btn btn-default login-btn">
+                <i class="fa fa-user"></i>登录
+            </a>
+            <RouterLink to="/auth/register" class="btn btn-default login-btn">
+                <i class="fa fa-user-plus"></i>注册
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TheEntry"
+    }
+</script>
+
+<style scoped>
+
+</style>
